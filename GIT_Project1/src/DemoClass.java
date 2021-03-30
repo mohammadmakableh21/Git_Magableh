6a8e56ee7ddb9ba3b55d6668cd16ca7e5fd2edd8
pc <pc@DESKTOP-IFVN841>
2021-03-30 21:39:44 +0300

    First update - magableh

--- GIT_Project1/src/DemoClass.java
+++ GIT_Project1/src/DemoClass.java
@@ -1,6 +1,24 @@
+import java.awt.Image;
+import java.awt.TextArea;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class DemoClass {
 
+	private JFrame frame;
+	TextArea textArea_1;
+	String s;
+	JLabel pic;
+	JLabel pic2 = null;
+	
+	public DemoClass() {
+		
+	//yyfytf
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World GIt");
